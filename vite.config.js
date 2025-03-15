de72eee7542c4f7b28d8cfa77ddafec5d6ce7e8c
JOfares <fares.albanna@outlook.com>
2025-03-15 07:42:30 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  root: ".", // تأكد من أن Vite يبدأ من المجلد الرئيسي
+  plugins: [react()], // تمكين React و JSX
   build: {
     rollupOptions: {
-      input: "public/index.html", // تحديد مسار index.html
+      input: "public/index.html",
     },
   },
 });
